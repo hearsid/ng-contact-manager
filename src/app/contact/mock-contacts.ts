d7d65eeeae46c7178c83541afae18073e07c528f
Siddharth Sharma <siddharth.sharma@kongsbergdigital.com>
2022-10-23 22:09:29 +0530

    styling changes added with default contact display

--- src/app/contact/mock-contacts.ts
+++ src/app/contact/mock-contacts.ts
@@ -4,36 +4,36 @@ export const CONTACTS: IContact[] = [
         id: 1,
         name : 'Terrence S. Hatfield',
         tel: '651-603-1723',
-        email: 'TerrenceSHatfield@rhyta.com'
+        email: 'TerrenceSHatfield@mail.com'
     },
     {
         id: 2,
         name : 'Chris M. Manning',
         tel: '513-307-5859',
-        email: 'ChrisMManning@dayrep.com'
+        email: 'ChrisMManning@mail.com'
     },
     {
         id: 3,
         name : 'Ricky M. Digiacomo',
-        tel: '918-774-0199',
-        email: 'RickyMDigiacomo@teleworm.us'
+        tel: '918-774-019',
+        email: 'RickyMDigiacomo@mail.com'
     },
     {
         id: 4,
         name : 'Michael K. Bayne',
         tel: '702-989-5145',
-        email: 'MichaelKBayne@rhyta.com'
+        email: 'MichaelKBayne@mail.com'
     },
     {
         id: 5,
         name : 'John I. Wilson',
         tel: '318-292-6700',
-        email: 'JohnIWilson@dayrep.com'
+        email: 'JohnIWilson@mail.com'
     },
     {
         id: 6,
         name : 'Rodolfo P. Robinett',
         tel: '803-557-9815',
-        email: 'RodolfoPRobinett@jourrapide.com'
+        email: 'RodolfoPRobinett@mail.com'
     }
 ];
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,69 +1,70 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import {Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 import { IContact } from '../contact/contact.model';
 import { ContactService } from '../contact/contact.service';
 @Component({
-    selector: 'contacts', 
-    templateUrl: './contacts.component.html'
+  selector: 'contacts',
+  templateUrl: './contacts.component.html'
 })
-export class ContactsComponent implements OnInit, OnDestroy{
+export class ContactsComponent implements OnInit, OnDestroy {
 
-    title = 'List of contacts';
-    contacts: IContact[];
-    selectedContact: IContact;
-    private subscription: Subscription;
-    private total_contacts : number;
+  title = 'Contacts';
+  contacts: IContact[];
+  selectedContact: IContact;
+  private subscription: Subscription;
+  private total_contacts: number;
 
-    constructor(private router: Router,
-                private contactService: ContactService,
-                private route: ActivatedRoute) {
+  constructor(private router: Router,
+    private contactService: ContactService,
+    private route: ActivatedRoute) {
 
-    }
-    ngOnInit() {
+  }
+  ngOnInit() {
     // subscribe to router event
-                      this.subscription = this.route.queryParams.subscribe(
-                        (param: Params) => {
-                          let total_contacts = +param['no_of_contacts']; // toInt
-                          this.total_contacts = Number(total_contacts);
-                          this.contacts = this.contactService.generateContacts(this.total_contacts);
-                          this.getContacts(this.total_contacts);
-                          console.log(this.total_contacts);
-                          console.log(this.contacts);
-                        });
+    this.subscription = this.route.queryParams.subscribe(
+      (param: Params) => {
+        let total_contacts = +param['no_of_contacts']; // toInt
+        this.total_contacts = Number(total_contacts);
+        this.contacts = this.contactService.generateContacts(this.total_contacts);
+        this.getContacts(this.total_contacts);
+        console.log(this.total_contacts);
+        console.log(this.contacts);
+      });
   }
 
   ngOnDestroy() {
     // prevent memory leak by unsubscribing
     this.subscription.unsubscribe();
   }
-    getContacts(total: number ): void {
-      //  this.contactService.getContacts().then(contacts => { this.contacts = contacts });
 
-      this.contactService.getContactList(total)
+  getContacts(total: number): void {
+    //  this.contactService.getContacts().then(contacts => { this.contacts = contacts });
+    const contactCount = total || 10;
+    this.contactService.getContactList(contactCount)
       .subscribe(
-        (contacts: IContact[])  => {
-        this.contactService.contacts = contacts ;
-        this.contacts = contacts;
+        (contacts: IContact[]) => {
+          this.contactService.contacts = contacts;
+          this.contacts = contacts;
         },
         (error: any) => console.error('Error: ' + error),
         () => console.log('Completed!')
       );
-    }
+  }
 
-    onSelect(contact: IContact): void {
-        this.selectedContact = contact;
-    }
+  onSelect(contact: IContact): void {
+    this.selectedContact = contact;
+  }
 
-    delete(index: number): void {
-        this.contactService.deleteContact(index);
+  delete(index: number): void {
+    this.contactService.deleteContact(index);
     //    this.getContacts();
-    }
+  }
 
-    addContact(): void {
-      // set active component to null to determine edit/add
-      this.contactService.activeContact = new IContact() ;
-      this.router.navigate(['/newContact']);
-    }
+  addContact(): void {
+    // set active component to null to determine edit/add
+    this.contactService.activeContact = new IContact();
+    this.router.navigate(['/newContact']);
+  }
 }
